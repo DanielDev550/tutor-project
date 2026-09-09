762dd99960c5ef450cc49eced7a8a18e6e65ce2d
Daniel Moses <danielmoses4714@gmail.com>
2026-09-09 13:59:41 -0700

    First commit: added console log assignment

---
+++ index.js
@@ -0,0 +1,4 @@
+const name ="Daniel"
+const age = 23
+const favouriteprogramminglanguage = "JavaScript"
+console.log (name, "is", age , "years old and his favourite programming language is", favouriteprogramminglanguage)
